d148caa74315e79cec791bbb2b232eb6a77fda8c
Anatoliy <staheev.a.n@yandex.ru>
2022-12-18 15:34:37 +0600

    написал метод переборки массива

--- Program.cs
+++ Program.cs
@@ -4,3 +4,28 @@
 //[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 //[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 //[“Russia”, “Denmark”, “Kazan”] → []
+
+
+void FilterArray(string[] array1, int index = 0)
+{
+    if (index < array1.Length)
+    {
+        Console.WriteLine(array1[index]);
+        FilterArray(array1, index + 1);
+    }
+    else
+    {
+        Console.WriteLine();
+    }
+
+}
+
+
+string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] array3 = new string[3] {"Russia", "Denmark", "Kazan"};
+
+
+Console.WriteLine(FilterArray(array1));
+Console.WriteLine(FilterArray(array2));
+Console.WriteLine(FilterArray(array3));
